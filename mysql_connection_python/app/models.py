482a42797e6e01629ab0c35b255874a9530b9d40
vToshio <vtoshio2004@gmail.com>
2024-10-09 21:12:03 -0300

    fix: correção de padronização e de erro de definição de nome da tabela "users".

--- mysql_connection_python/app/models.py
+++ mysql_connection_python/app/models.py
@@ -4,7 +4,8 @@ from __init__ import db
     Classe deve ter o mesmo nome da tabela relacional e seus
 atributos devem tem os mesmos nomes de cada coluna da tabela 
 '''
-class Users(db.Model):
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(30), nullable=False)
     role = db.Column(db.String(20), nullable=False)
--- mysql_connection_python/app/views.py
+++ mysql_connection_python/app/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
-from models import Users
+from models import User
 
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
 def index():
-    user_list = Users.query.order_by(Users.id)
+    user_list = User.query.order_by(User.id)
     return render_template('index.html', nome='teste.py', users_list=user_list) 
